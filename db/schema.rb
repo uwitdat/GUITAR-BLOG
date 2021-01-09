c08a27aad17030e0ad398d7de4ac947b51c67a4b
Ben Shekhtman <benshekhtman@hotmail.com>
2021-01-08 21:20:47 -0500

    Add one-to-many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_002802) do
+ActiveRecord::Schema.define(version: 2021_01_09_020548) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
